73d856324dff278e025f2b9b990c264facc64bf4
parikshitdas10 <parikshitdas10@gmail.com>
2022-06-11 10:36:06 +0530

    Added new files

---
+++ UDPCalc_Server.java
@@ -0,0 +1,78 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.StringTokenizer;
+
+
+class UDP_Server {
+
+	public static void main(String[] args)
+		throws IOException
+	{
+
+		DatagramSocket ds = new DatagramSocket(1234);
+
+		byte[] buf = null;
+
+		DatagramPacket DpReceive = null;
+		DatagramPacket DpSend = null;
+
+		while (true) {
+			buf = new byte[65535];
+
+			DpReceive = new DatagramPacket(buf, buf.length);
+
+			ds.receive(DpReceive);
+
+			String inp = new String(buf, 0, buf.length);
+
+			inp = inp.trim();
+
+			System.out.println("Equation Received:- "
+							+ inp);
+
+			if (inp.equals("bye")) {
+				System.out.println(
+					"Client sent bye.....EXITING");
+				break;
+			}
+
+			int result;
+
+			
+			StringTokenizer st = new StringTokenizer(inp);
+
+			int oprnd1 = Integer.parseInt(st.nextToken());
+			String operation = st.nextToken();
+			int oprnd2 = Integer.parseInt(st.nextToken());
+
+		
+			if (operation.equals("+"))
+				result = oprnd1 + oprnd2;
+
+			else if (operation.equals("-"))
+				result = oprnd1 - oprnd2;
+
+			else if (operation.equals("*"))
+				result = oprnd1 * oprnd2;
+
+			else
+				result = oprnd1 / oprnd2;
+
+			System.out.println("Sending the result...");
+			String res = Integer.toString(result);
+
+			
+			buf = res.getBytes();
+
+			
+			int port = DpReceive.getPort();
+
+			DpSend = new DatagramPacket(
+				buf, buf.length, InetAddress.getLocalHost(),
+				port);
+			ds.send(DpSend);
+		}
+	}
+}
